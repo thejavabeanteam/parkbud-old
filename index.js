7cca2d4b69e529e677e7e2075ddac080540fdd57
Mora <mlabisi98@gmail.com>
2019-09-25 14:47:00 -0700

    Added run script and terminated lines in index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const axios = require('axios');
 
@@ -15,7 +15,7 @@ async function getNumberOfFollowers() {
   let nOfFollowers = res.data.followers;
   let location = res.data.location;
 
-  console.log(`# of followers: ${nOfFollowers}`)
+  console.log(`# of followers: ${nOfFollowers}`);
   console.log(`Location: ${location}`)
 }
 getNumberOfFollowers();
@@ -27,7 +27,7 @@ async function makePostRequest() {
         first_name: 'Fred',
         last_name: 'Blair',
         email: 'freddyb34@gmail.com'
-      }
+      };
 
     let res = await axios.post('http://localhost:3000/users/', params);
 
