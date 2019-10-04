2ecf38565ab1029a6325d21dbd3e7ad7ebd2296e
Mora <mlabisi98@gmail.com>
2019-10-04 13:20:24 -0700

    Fixed typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json();)
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
  res.send('Hello World')
